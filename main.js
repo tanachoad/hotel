b271285b722244ad570c554583b1196b6557d28a
tanachoad <tanachoad.ar@mail.wu.ac.th>
2023-06-30 09:44:47 +0700

    Update main.js
    เพิ่มตัวคลิก

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const addForm = document.getElementById("add-user-form");
 const showAlert = document.getElementById("showAlert");
 const addModal = new bootstrap.Modal(document.getElementById("addNewUserModal"));
 const tbody = document.querySelector("tbody");
+const updateForm = document.getElementById("edit-user-form");
+const editModal = new bootstrap.Modal(document.getElementById("editUserModal"));
+// const generatePdfBtn = document.getElementById("generate-pdf-btn");
 
 addForm.addEventListener("submit", async (e)=>{
     e.preventDefault();
@@ -62,4 +65,64 @@ const editUser = async (id) =>{
     document.getElementById("electricity_bill").value = response.electricity_bill;
     document.getElementById("water_bill").value = response.water_bill;
     document.getElementById("parking_fee").value = response.parking_fee;
+}
+
+updateForm.addEventListener("submit", async (e) =>{
+    e.preventDefault();
+
+    const formData = new FormData(updateForm);
+    formData.append("update", 1);
+
+    if(updateForm.checkValidity() === false) {
+        e.preventDefault();
+        e.stopPropagation();
+        addForm.classList.add("was-validated");
+        return false;
+    } else {
+        document.getElementById("edit-user-btn").value = "Please Wait...";
+
+        const data = await fetch("action.php", {
+            method: "POST",
+            body: formData
+        });
+        const response = await data.text();
+        showAlert.innerHTML = response;
+        document.getElementById("edit-user-btn").value = "Edit User";
+        updateForm.reset();
+        updateForm.classList.remove("was-validated");
+        editModal.hide();
+        fetchAllUsers();
+    }
+});
+
+// c
+// generatePdfBtn.addEventListener("click", () => {
+//     generatePdf();
+// });
+
+// const generatePdf = async () => {
+//     const data = await fetch("action.php?pdf=1", {
+//         method: "GET"
+//     });
+//     // Handle the response or any error if needed
+//     // For example, you can open the generated PDF in a new tab
+//     window.open(data.url, "_blank");
+// };
+
+// delete
+tbody.addEventListener("click", (e) => {
+    if (e.target && e.target.matches("a.deletelink")) {
+        e.preventDefault();
+        let id = e.target.getAttribute("id");
+        deleteUser(id);
+    }
+});
+
+const deleteUser = async (id) => {
+    const data = await fetch(`action.php?delete=1&id=${id}`, {
+        method: "GET"
+    });
+    const response = await data.text();
+    showAlert.innerHTML = response;
+    fetchAllUsers();
 }
